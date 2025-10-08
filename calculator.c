c1738f0783b06ccaee3a27d8af255f0e478dba9d
vatsal <vatsalvashishtha6@gmail.com>
2025-10-08 22:01:59 +0530

    Added functional comments and also upgraded code for different test cases

--- calculator.c
+++ calculator.c
@@ -7,10 +7,22 @@ int value_stack[256];
 char op_stack[256];
 int numTop = -1, opTop = -1;
 
+/*
+getOperatorPrecedence :
+    This function return the precedence of an operator.
+    Like * and / will have higher precedence which is (2).
+    While + and - will have lower precedence of (1). 
+*/
 int getOperatorPrecedence(char op) {
     return (op == '*' || op == '/') ? 2 : 1;
 }
 
+/*
+applyOperator:
+    This function applies a given operator to two integer operands.
+    Also handles division by zero and invalid operators.
+    And it returns the result of applying the operator.
+*/
 int applyOperator(int val1, int val2, char op, int *errorFlag) {
     if (op == '/' && val2 == 0) {
         *errorFlag = 2;
@@ -27,6 +39,11 @@ int applyOperator(int val1, int val2, char op, int *errorFlag) {
     }
 }
 
+/*
+calculate:
+    This function evaluates a simple arithmatic expression.
+    Also it will handle invalid input, operator precedence and division by zero.
+*/
 void calculate(char* expr) {
     char* p = expr;
     int errorFlag  = 0;
@@ -62,6 +79,10 @@ void calculate(char* expr) {
             }
         }
         if (strchr("+-*/", *p)) {
+            if(p == expr || strchr("+-*/", *(p-1))){
+                errorFlag = 1;
+                break;
+            }
             op_stack[++opTop] = *p++;
         } else {
             errorFlag  = 1;
@@ -89,6 +110,10 @@ void calculate(char* expr) {
     }
 }
 
+/*
+    The main function reads an arithmatic expression from the input by user.
+    Also it removes newline and calls calculate() to evaluate it.
+*/
 int main() {
     char userInput[1024];
     if (fgets(userInput, sizeof(userInput), stdin)) {
